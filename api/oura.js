e2c91702f3d624873e6d1970b6d8aafeefb08fd2
umesh047648-pixel <umesh047648@gmail.com>
2026-01-28 18:31:00 +1100

    Update oura.js

--- api/oura.js
+++ api/oura.js
@@ -1,1 +1,22 @@
+export default async function handler(req, res) {
+  try {
+    if (!process.env.OURA_TOKEN) {
+      return res.status(500).json({ error: "OURA_TOKEN not set" });
+    }
+
+    const response = await fetch(
+      "https://api.ouraring.com/v2/usercollection/readiness",
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.OURA_TOKEN}`,
+        },
+      }
+    );
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
 
